6902b9885ba2206365a47273f090d72e61fd03d5
takumma <taku.3001@icloud.com>
2022-03-19 10:16:15 +0900

    Add Linaria

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const withLinaria = require("next-linaria")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withLinaria(nextConfig)
