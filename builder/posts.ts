78c7ce6b66d30c634dd73fd701b627d3d7e1b8d2
takumma <taku.3001@icloud.com>
2022-04-17 04:08:42 +0900

    add post builder

---
+++ builder/posts.ts
@@ -0,0 +1,67 @@
+import fs from "fs-extra";
+import Parser from "rss-parser";
+
+const sources = [
+  "https://zenn.dev/takumma/feed",
+  "https://qiita.com/takumma/feed.atom",
+  "https://blog.takumma.net/feed",
+];
+
+type FeedItem = {
+  title: string;
+  link: string;
+  contentSnippet?: string;
+  isoDate?: string;
+  dateMiliSeconds: number;
+};
+
+function isValidUrl(str: string): boolean {
+  try {
+    const { protocol } = new URL(str);
+    return protocol === "http:" || protocol === "https:";
+  } catch {
+    return false;
+  }
+}
+
+const parser = new Parser();
+
+async function fetchFeedItems(url: string) {
+  const feed = await parser.parseURL(url);
+  if (!feed?.items?.length) return [];
+
+  // return item which has title and link
+  return feed.items
+    .map(({ title, contentSnippet, link, isoDate }) => {
+      return {
+        title,
+        contentSnippet: contentSnippet?.replace(/\n/g, ""),
+        link,
+        isoDate,
+        dateMiliSeconds: isoDate ? new Date(isoDate).getTime() : 0,
+      };
+    })
+    .filter(
+      ({ title, link }) => title && link && isValidUrl(link)
+    ) as FeedItem[];
+}
+
+async function getFeedItemsFromSources(sources: undefined | string[]) {
+  if (!sources?.length) return [];
+  let feedItems: FeedItem[] = [];
+  for (const url of sources) {
+    const items = await fetchFeedItems(url);
+    if (items) feedItems = [...feedItems, ...items];
+  }
+
+  if (!feedItems) return [];
+
+  return feedItems;
+}
+
+(async function () {
+  const feedItems = await getFeedItemsFromSources(sources);
+  feedItems.sort((a, b) => b.dateMiliSeconds - a.dateMiliSeconds);
+  fs.ensureDirSync(".contents");
+  fs.writeJsonSync(".contents/posts.json", feedItems);
+})();
