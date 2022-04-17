bdead754b78260be012b25a2d177bf9ad23092fc
takumma <taku.3001@icloud.com>
2022-04-17 21:57:12 +0900

    Fix hook

---
+++ hooks/useArticles.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { Article } from "../types/article";
+import usePagenation from "./usePagenation";
+
+export const useArticles = (articles: Article[]) => {
+  const [currentArticles, setCurrentArticles] = useState<Article[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const PAGE_SIZE = 10;
+
+  const { pagenationRange } = usePagenation({
+    totalCount: articles.length,
+    pageSize: PAGE_SIZE,
+  });
+
+  useEffect(() => {
+    const startPageIndex = (currentPage - 1) * PAGE_SIZE;
+    const endPageIndex = startPageIndex + PAGE_SIZE;
+    const newArticles = articles.slice(startPageIndex, endPageIndex);
+    setCurrentArticles(newArticles);
+  }, [articles, currentPage]);
+
+  return { currentArticles, currentPage, pagenationRange, setCurrentPage };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,22 @@
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
 import styled from "styled-components";
 import ArticleList from "../components/common/Article/ArticleList";
 import Pagenation from "../components/common/Pagenation/Pagenation";
-import usePagenation from "../components/common/Pagenation/usePagenation";
-import { Article } from "../types/article";
 import articles from "../.contents/articles.json";
+import { useArticles } from "../hooks/useArticles";
 
 const Home: NextPage = () => {
-  const [trimedArticles, setTrimedArticles] = useState<Article[]>();
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const PAGE_SIZE = 10;
-
-  const { pagenationRange } = usePagenation({
-    totalCount: articles.length,
-    pageSize: PAGE_SIZE,
-  });
-
-  useEffect(() => {
-    const startPageIndex = (currentPage - 1) * PAGE_SIZE;
-    const endPageIndex = startPageIndex + PAGE_SIZE;
-    const showArticles = (articles as Article[]).slice(
-      startPageIndex,
-      endPageIndex
-    );
-    setTrimedArticles(showArticles);
-  }, [currentPage]);
+  const { currentArticles, currentPage, pagenationRange, setCurrentPage } =
+    useArticles(articles);
 
   return (
     <>
       <Wrapper>
-        {trimedArticles && <ArticleList articles={trimedArticles} />}
+        {currentArticles && <ArticleList articles={currentArticles} />}
       </Wrapper>
       <PagenationWrapper>
         <Pagenation
           onPageChange={(v) => {
-            console.log(v);
             if (v < 1) return;
             setCurrentPage(v);
           }}
