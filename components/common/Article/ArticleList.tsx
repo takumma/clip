f4449b710c7133fc5c39eb84344312aabd7bf8c4
takumma <taku.3001@icloud.com>
2022-08-13 22:07:53 +0900

    fix style

--- components/common/Article/ArticleList.tsx
+++ components/common/Article/ArticleList.tsx
@@ -20,7 +20,7 @@ const ArticleListWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  align-items: flex-start;
+  align-items: center;
   ::after {
     display: block;
     content: "";
