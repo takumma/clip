5131a340c9dbfcf9efd240c9d71a370813739acb
takumma <taku.3001@icloud.com>
2023-05-28 21:26:20 +0900

    add note

--- feeds.ts
+++ feeds.ts
@@ -4,5 +4,6 @@ export const sources = [
   "https://blog.takumma.net/feed.xml",
   "https://takumma.hatenablog.com/rss",
   "https://scrapbox.io/api/feed/takumma",
+  "https://note.com/_takumma/rss",
 ];
 
